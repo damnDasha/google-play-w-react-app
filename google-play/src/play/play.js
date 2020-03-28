6d1baa78c40277687e80439f8f7d047bfd4bcd2c
dasha akhten <dasha.akhten@gmail.com>
2020-03-28 14:57:00 -0700

    props name needed to be capitalised

--- google-play/src/play/play.js
+++ google-play/src/play/play.js
@@ -5,10 +5,10 @@ export default function Play(props) {
   return (
     <div className="play">
       <h2>{props.app}</h2>
-      <div className="app name"> App name: {props.app}</div>
-      <div className="category">Category: {props.category}</div>
-      <div className="genre"> Genre {props.genres}</div>
-      <div className="rating">rating: {props.rating}</div>
+      <div className="app name"> App name: {props.App}</div>
+      <div className="category">Category: {props.Category}</div>
+      <div className="genre"> Genre {props.Genres}</div>
+      <div className="rating">rating: {props.Rating}</div>
     </div>
   );
 }
